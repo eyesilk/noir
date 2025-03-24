bb9f7b8ce75e4961aff8bab76d470b2ab0320f62
eyesilk <danila.stepanov2007@gmail.com>
2025-03-24 06:25:39 +0300

    https

--- backend/src/modules/auths/lib/setTokenToCookie.ts
+++ backend/src/modules/auths/lib/setTokenToCookie.ts
@@ -5,6 +5,6 @@ export default (res: Response, refreshToken: string): void => {
     maxAge: 32 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     secure: true,
-    sameSite: 'none',
+    sameSite: 'lax',
   });
 };
