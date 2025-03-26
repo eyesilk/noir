c7b7aef460f85621e721a7d3ea9b736d67627225
eyesilk <danila.stepanov2007@gmail.com>
2025-03-26 22:49:41 +0300

    trying to fix cookie

--- backend/src/modules/auths/lib/setTokenToCookie.ts
+++ backend/src/modules/auths/lib/setTokenToCookie.ts
@@ -4,7 +4,8 @@ export default (res: Response, refreshToken: string): void => {
   res.cookie('refreshToken', refreshToken, {
     maxAge: 32 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    secure: true,
-    sameSite: 'none',
+    secure: process.env.IS_SECURE === 'true',
+    sameSite: process.env.SAME_SITE as boolean | 'lax' | 'strict' | 'none',
+    domain: 'noir-three.vercel.app'
   });
 };
