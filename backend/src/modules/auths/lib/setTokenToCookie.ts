33640bbbb22d5db70874c8e8c93db9c31bff503c
eyesilk <danila.stepanov2007@gmail.com>
2025-03-26 23:27:36 +0300

    cookie

--- backend/src/modules/auths/lib/setTokenToCookie.ts
+++ backend/src/modules/auths/lib/setTokenToCookie.ts
@@ -4,7 +4,7 @@ export default (res: Response, refreshToken: string): void => {
   res.cookie('refreshToken', refreshToken, {
     maxAge: 32 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    secure: process.env.IS_SECURE === 'true',
+    secure: true,
     sameSite: 'none',
     // sameSite: process.env.SAME_SITE as boolean | 'lax' | 'strict' | 'none',
     // domain: 'noir-three.vercel.app'
