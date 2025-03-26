d34fa76d3a79e687629195db70e36a231c2495dd
eyesilk <danila.stepanov2007@gmail.com>
2025-03-27 02:44:44 +0300

    vercel config backend

--- backend/src/modules/auths/lib/setTokenToCookie.ts
+++ backend/src/modules/auths/lib/setTokenToCookie.ts
@@ -4,7 +4,7 @@ export default (res: Response, refreshToken: string): void => {
   res.cookie('refreshToken', refreshToken, {
     maxAge: 32 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    secure: true,
+    // secure: true,
     sameSite: process.env.SAME_SITE as boolean | 'lax' | 'strict' | 'none',
     path: '/',
   });
