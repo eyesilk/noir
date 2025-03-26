d7411d0d24d57f479e4fc6cdf423cd194640b591
eyesilk <danila.stepanov2007@gmail.com>
2025-03-26 23:21:46 +0300

    cookie ahh moment

--- backend/src/modules/auths/lib/setTokenToCookie.ts
+++ backend/src/modules/auths/lib/setTokenToCookie.ts
@@ -5,7 +5,8 @@ export default (res: Response, refreshToken: string): void => {
     maxAge: 32 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     secure: process.env.IS_SECURE === 'true',
-    sameSite: process.env.SAME_SITE as boolean | 'lax' | 'strict' | 'none',
+    sameSite: 'none',
+    // sameSite: process.env.SAME_SITE as boolean | 'lax' | 'strict' | 'none',
     // domain: 'noir-three.vercel.app'
   });
 };
