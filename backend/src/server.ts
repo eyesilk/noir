0d14362bef30e8416a7fd90a89c9ffce12224632
eyesilk <danila.stepanov2007@gmail.com>
2025-03-27 00:34:50 +0300

    cross middleware

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,19 +1,27 @@
-import { app } from "./app";
-import prisma from "./lib/prisma";
+import { app } from './app';
+import prisma from './lib/prisma';
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,refreshToken');
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+  res.header('Content-Type', 'application/json;charset=utf-8');
+  next();
+});
 
 const PORT = process.env.PORT || 5000;
 
 const start = async (): Promise<void> => {
   try {
     await prisma.$connect();
-    console.log("MongoDB OK.")
+    console.log('MongoDB OK.');
 
     app.listen(PORT, (): void => {
       console.log(`Server OK. Port: ${process.env.PORT}`);
     });
   } catch (err: unknown) {
-    console.log("Server shutdown with error: ", err);
+    console.log('Server shutdown with error: ', err);
   }
 };
 
-start()
+start();
