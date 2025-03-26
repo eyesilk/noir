d4e1aab62cd3754fa9e3e3e028fa98ce727dd7dd
eyesilk <danila.stepanov2007@gmail.com>
2025-03-27 00:46:02 +0300

    cors middleware

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,6 +6,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,refreshToken,token');
   res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
   res.header('Content-Type', 'application/json;charset=utf-8');
+  res.header('Access-Control-Allow-Credentials', 'true');
 
   if (req.method === 'OPTIONS') {
     res.sendStatus(200);
